fe6905110e4d3d68a46160cd2179ddbd1637ebe8
RandomRobin <rlvvlijmen@gmail.com>
2019-04-02 15:36:27 +0200

    Hotfix

--- TestApplicate/TestApplicate/TestApplicate.Android/MainActivity.cs
+++ TestApplicate/TestApplicate/TestApplicate.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace TestApplicate.Droid
 {
-    [Activity(Label = "TestApplicate", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "testApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
