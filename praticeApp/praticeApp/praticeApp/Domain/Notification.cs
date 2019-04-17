98658e84b5a6e088cb5aacc5ccd975ebcb14250c
lhuijgens <lucca.huijgens@student.hu.nl>
2019-04-17 13:05:43 +0200

    Final UI for Feed and Notification Details

--- praticeApp/praticeApp/praticeApp/Domain/Notification.cs
+++ praticeApp/praticeApp/praticeApp/Domain/Notification.cs
@@ -11,5 +11,24 @@ namespace praticeApp.Domain
         public String Sender { get; set; }
         public DateTime Date { get; set; }
 
+        public string dateToString()
+        {
+            if (DateTime.Now.ToString("MM/dd/yyyy").Equals(Date.ToString("MM/dd/yyyy")))
+            {
+                return Date.ToString("HH:mm");
+            }
+            else if (DateTime.Today - Date.Date == TimeSpan.FromDays(1))
+            {
+                return "Yesterday";
+            }
+            else if (DateTime.Today.Year != Date.Year)
+            {
+                return Date.ToString("dd/MM/yyyy");
+            }
+            else
+            {
+                return Date.ToString("dd MMM");
+            }
+        }
     }
 }
