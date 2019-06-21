7e082ba83e84d1a7d9e3c6a3b410a531380dde2f
lhuijgens <lucca.huijgens@student.hu.nl>
2019-06-21 10:38:48 +0200

    change submitQuestion from httpwebrequest to httpclient

--- praticeApp/praticeApp/praticeApp/DataAccess/ConfigLoader.cs
+++ praticeApp/praticeApp/praticeApp/DataAccess/ConfigLoader.cs
@@ -20,25 +20,11 @@ namespace praticeApp.DataAccess
         {
             if (File.Exists(GetFileName()))
             {
-                //token = File.ReadAllText(GetFileName());
-                token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjBjNjU5OGQ1MmJhYmI3YzQ1ZWU3NDAyNzg4YzNmNWFiY2I3OGJlNDMzMGQxNjZjMGQ0ZTY1MTVhODU4MjM0OWUzZjUwYmQwZjk5MDIzMjc2In0.eyJhdWQiOiIzIiwianRpIjoiMGM2NTk4ZDUyYmFiYjdjNDVlZTc0MDI3ODhjM2Y1YWJjYjc4YmU0MzMwZDE2NmMwZDRlNjUxNWE4NTgyMzQ5ZTNmNTBiZDBmOTkwMjMyNzYiLCJpYXQiOjE1NjEwMjE4NzIsIm5iZiI6MTU2MTAyMTg3MiwiZXhwIjoxNTkyNjQ0MjcyLCJzdWIiOiI4Iiwic2NvcGVzIjpbXX0.i_4P73LbxpvU8IyniGteeNy2fAYRXCBt-drh1C1-4TKhwmQRCQrTjuQ7AJcIIjKLxAJpbzrEcK6aZoLppJ3nSQ";
+                token = File.ReadAllText(GetFileName());
                 return true;
             }
 
             return false;
-        }
-
-        public String LoadConfigTokenOutFile()
-        {
-            /**if (File.Exists(GetFileName()))
-            {
-                String token = File.ReadAllText(GetFileName());
-                return token;
-            }
-
-            return "";**/
-
-            return "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjBjNjU5OGQ1MmJhYmI3YzQ1ZWU3NDAyNzg4YzNmNWFiY2I3OGJlNDMzMGQxNjZjMGQ0ZTY1MTVhODU4MjM0OWUzZjUwYmQwZjk5MDIzMjc2In0.eyJhdWQiOiIzIiwianRpIjoiMGM2NTk4ZDUyYmFiYjdjNDVlZTc0MDI3ODhjM2Y1YWJjYjc4YmU0MzMwZDE2NmMwZDRlNjUxNWE4NTgyMzQ5ZTNmNTBiZDBmOTkwMjMyNzYiLCJpYXQiOjE1NjEwMjE4NzIsIm5iZiI6MTU2MTAyMTg3MiwiZXhwIjoxNTkyNjQ0MjcyLCJzdWIiOiI4Iiwic2NvcGVzIjpbXX0.i_4P73LbxpvU8IyniGteeNy2fAYRXCBt-drh1C1-4TKhwmQRCQrTjuQ7AJcIIjKLxAJpbzrEcK6aZoLppJ3nSQ";
         }
 
         public void WriteConfigTokenInFile(String token)
