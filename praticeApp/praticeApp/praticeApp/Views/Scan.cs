51bb8d8ebaa169b4244bd87bbb87f83fa5b3837e
RandomRobin <rlvvlijmen@gmail.com>
2019-06-21 09:06:17 +0200

    Scan upgrade

--- praticeApp/praticeApp/praticeApp/Views/Scan.cs
+++ praticeApp/praticeApp/praticeApp/Views/Scan.cs
@@ -9,48 +9,83 @@ using ZXing.Net.Mobile.Forms;
 
 namespace praticeApp.Views
 {
-    public partial class Scan : ZXingScannerPage
+    public partial class Scan : ContentPage
     {
+        ZXingScannerView zxing;
+        ZXingDefaultOverlay overlay;
+        public int scanTries;
 
-        public Scan()
+        public Scan() : base()
         {
-            var animat = new Animation();
-            this.Animate("Qr", animat, 5, 100, null, null, null);
-            IsScanning = true;
-            ScanAsync();
-        }
-
-        public void ScanAsync()
-        {
-            OnScanResult += (result) =>
+            zxing = new ZXingScannerView
             {
-
-
-                if (ScanController.ProcessResult(result.Text))
-                {
-
-                    Debug.WriteLine("\nScans Succeed...\n");
-                    Device.BeginInvokeOnMainThread(async () =>
-                    {
-                        await DisplayAlert("Succes~!", "Je account is succesvol gekoppeld", "Begrepen.");
-                    });
-
-                    App.Current.MainPage = new NavigationPage(new NavMaster());
-
-
-                }
-                else
-                {
-
-                    Debug.WriteLine("\nScanfailded...\n");
-                }
-
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
             };
 
+            zxing.OnScanResult += (result) =>
+            {
+                while (scanTries <= 5)
+                {
+                    if (ScanController.ProcessResult(result.Text))
+                    {
+                        Debug.WriteLine("\nScans Succeed...\n");
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            await DisplayAlert("Succes~!", "Je account is succesvol gekoppeld", "Begrepen.");
+                        });
 
+                        App.Current.MainPage = new NavigationPage(new NavMaster());
+                    }
+                    else
+                    {
+
+                        Debug.WriteLine("\nScanfailded...\n");
+                        scanTries = (scanTries + 1);
+                        overlay.TopText = "Poging: " + scanTries;
+                    }
+                }
+                
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await DisplayAlert("Fout~!", "Je account kan niet worden gekoppeld, controleer je internetverbinding of probeer het later opnieuw.", "Begrepen.");
+                    });
+                
+
+};
+            overlay = new ZXingDefaultOverlay
+            {
+                TopText = "Poging: " + scanTries,
+                BottomText = "Even geduld a.u.b.",
+                ShowFlashButton = zxing.HasTorch,
+            };
+            overlay.FlashButtonClicked += (sender, e) => {
+                zxing.IsTorchOn = !zxing.IsTorchOn;
+            };
+            var grid = new Grid
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+            grid.Children.Add(zxing);
+            grid.Children.Add(overlay);
+
+            Content = grid;
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
 
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
     }
 }
 
