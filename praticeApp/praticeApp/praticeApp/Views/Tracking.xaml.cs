d74af1724eb20ea379ed890a72a02bba4e2f7aba
RandomRobin <rlvvlijmen@gmail.com>
2019-06-25 14:58:04 +0200

    The background update - Added files for background function

---
+++ praticeApp/praticeApp/praticeApp/Views/Tracking.xaml.cs
@@ -0,0 +1,23 @@
+﻿using praticeApp.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace praticeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tracking : ContentPage
+    {
+        public Tracking()
+        {
+            InitializeComponent();
+        }
+
+        public void ActivateTracking(object sender, EventArgs e)
+        {
+            TrackingController.switchTracking();
+        }
+    }
+}
